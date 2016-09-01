ca33657c2e4f5737719c634ba45bff45b1bcc0f4
Matthew Kosoy <matt.kosoy@gmail.com>
2016-08-31 22:08:34 -0700

    animations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,6 +27,9 @@ Rails.application.configure do
   # number of complex assets.
   config.assets.debug = true
 
+  # Compile the assets
+  config.assets.compile = true
+
   # Adds additional error checking when serving assets at runtime.
   # Checks for improperly declared sprockets dependencies.
   # Raises helpful error messages.
